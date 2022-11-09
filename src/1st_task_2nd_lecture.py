f42b213c8c7bc3ed2f7d9d000c213561f0a67a34
KateVoropai <katevoropai@gmail.com>
2022-11-09 23:10:46 +0300

    Added function def modified_tuple_2

--- src/1st_task_2nd_lecture.py
+++ src/1st_task_2nd_lecture.py
@@ -4,10 +4,21 @@ def modified_tuple(some_tuple):
             if num % 2 == 0:
                 some_list.append(num * 2)
             else:
-                some_list.append(2)
+                some_list.append(num - 2)
     return tuple(some_list)
-                
+
+def modified_tuple_2(some_tuple):
+    modified_tuple = ()
+    for num in some_tuple:
+            if num % 2 == 0:
+                new_tuple = (num * 2,)
+                modified_tuple += new_tuple
+            else:
+                new_tuple = (num - 2,)
+                modified_tuple += new_tuple
+    return modified_tuple
 
 entered_list = input("Введите список чисел: ").split()
 my_list = [int(i) for i in entered_list]
 print(modified_tuple(tuple(my_list)))
+print(modified_tuple_2(tuple(my_list)))
