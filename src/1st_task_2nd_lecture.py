b566af445df44f0aadac9edfa9656bee27e3742c
KateVoropai <katevoropai@gmail.com>
2022-11-15 06:48:37 +0300

    Changed def modified_tuple_2

--- src/1st_task_2nd_lecture.py
+++ src/1st_task_2nd_lecture.py
@@ -7,18 +7,15 @@ def modified_tuple(some_tuple):
                 some_list.append(num - 2)
     return tuple(some_list)
 
-def modified_tuple_2(some_tuple):
-    modified_tuple = ()
-    for num in some_tuple:
-            if num % 2 == 0:
-                new_tuple = (num * 2,)
-                modified_tuple += new_tuple
-            else:
-                new_tuple = (num - 2,)
-                modified_tuple += new_tuple
-    return modified_tuple
+def modified_tuple_2(some_tuple):      #2 вариант решения, без создания some_list=[] 
+    for num in range(len(some_tuple)):
+        if some_tuple[num] % 2 == 0:
+            some_tuple[num] = some_tuple[num] * 2
+        else:
+            some_tuple[num] = some_tuple[num] - 2
+    return tuple(some_tuple)
 
 entered_list = input("Введите список чисел: ").split()
 my_list = [int(i) for i in entered_list]
 print(modified_tuple(tuple(my_list)))
-print(modified_tuple_2(tuple(my_list)))
+print(modified_tuple_2(my_list))
