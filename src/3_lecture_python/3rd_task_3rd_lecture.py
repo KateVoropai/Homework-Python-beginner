4421dafa283b1673bc4169644e134ae3b1ed4d00
KateVoropai <katevoropai@gmail.com>
2022-12-13 15:54:19 +0300

    Added changes in 3rd_task_3rd_lecture

--- src/3_lecture_python/3rd_task_3rd_lecture.py
+++ src/3_lecture_python/3rd_task_3rd_lecture.py
@@ -1,24 +1,22 @@
-def find_secret_number(secret, guess, A, B):
-    guess_nums = []
-    secret_nums = []
+def input_numbers():
+    val = True
+    while val:
+        secret_number = input("Введите секретное число ")
+        guess_number = input("Введите предполагаемое число ")
+        if len(guess_number) != len(secret_number):
+            print('Please, input correct data.')
+        else:
+            val = False
+    return secret_number, guess_number
+
+def find_secret_number(secret, guess, A = 0, B = 0):
     for i in range(len(guess)):
         if secret[i] == guess[i]:
             A += 1
-        elif secret[i] != guess[i]:
-            secret_nums.append(secret[i])
-            guess_nums.append(guess[i])
-    if len(guess_nums) > 0:
-        for j in guess_nums[::-1]:
-            if j in secret_nums:
-                B += 1
-                guess_nums.remove(j)
-                secret_nums.remove(j)            
+        if guess[i] in secret and secret[i] != guess[i]:
+            B += 1
     return f"{A}A{B}B"
     
-secret_number = input("Введите секретное число ")
-guess_number = input("Введите предполагаемое число ")
-A = 0
-B = 0
-if len(guess_number) < len(secret_number) or len(guess_number) > len(secret_number):
-    guess_number = input(f"Секретное число {len(secret_number)} значное, введите равнозначное число ")
-print(find_secret_number(list(secret_number), list(guess_number), A, B))
+
+secret_number, guess_number = input_numbers()
+print(find_secret_number(secret_number, guess_number))
