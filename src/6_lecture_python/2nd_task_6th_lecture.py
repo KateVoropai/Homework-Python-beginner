7ac3f586bda714a0b8a1dc97ee755f9f132f03be
KateVoropai <katevoropai@gmail.com>
2022-12-26 08:29:20 +0300

    Added changes

--- src/6_lecture_python/2nd_task_6th_lecture.py
+++ src/6_lecture_python/2nd_task_6th_lecture.py
@@ -1,6 +1,9 @@
 def delete_duplicate(my_list: list[int]) -> list[int]:
-    list_without_duplicate = list(set(my_list))
-    return list_without_duplicate 
+    without_duplicate = {}
+    for i in my_list:
+        without_duplicate[i] = i
+    
+    return [i for i in without_duplicate.keys()]
            
 entered_list = input("Введите список чисел: ").split()
 my_list = [int(i) for i in entered_list]
